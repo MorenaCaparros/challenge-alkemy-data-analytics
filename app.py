9a5c993c1306e5ed9d133b5050d775c21020f36e
MorenaCaparros <morens217@gmail.com>
2022-08-09 16:11:53 -0300

    Get CSV from API

---
+++ app.py
@@ -0,0 +1,22 @@
+import requests
+
+museos = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'
+cine = "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv"
+bibliotecas = "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv"
+
+def cc(archive_name):
+    with open(archive_name, encoding='utf-8') as archivo:
+        next(archivo)
+        alumnos = []
+        
+try:
+    separador = ','
+    r = requests.get(museos)
+    cc(r)
+        
+    
+    print()
+
+except:
+    print("Invalid URL or some error occured while making the GET request to the specified URL")
+
