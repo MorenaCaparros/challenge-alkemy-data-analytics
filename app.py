d2921903267095d40363bcd63994bc42a889d566
MorenaCaparros <morens217@gmail.com>
2022-08-10 13:27:43 -0300

    test file system

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import requests
+import requests as req
 
-museos = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'
-cine = "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv"
-bibliotecas = "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv"
+museum = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'
+cinema = "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv"
+popularlibraries = "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv"
 
 def cc(archive_name):
     with open(archive_name, encoding='utf-8') as archivo:
@@ -10,13 +10,7 @@ def cc(archive_name):
         alumnos = []
         
 try:
-    separador = ','
-    r = requests.get(museos)
-    cc(r)
-        
-    
-    print()
-
+    file = req.get(museos)
 except:
     print("Invalid URL or some error occured while making the GET request to the specified URL")
 
