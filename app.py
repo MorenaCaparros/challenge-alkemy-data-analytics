8370cda8fffc0ad7e31260c948ff84797c1bdb18
MorenaCaparros <morens217@gmail.com>
2022-08-14 01:43:43 -0300

    Change saveCSVFile function  and fix setFolderAndFiles function

--- app.py
+++ app.py
@@ -10,16 +10,17 @@ locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')  # cambia el idioma
 x = datetime.datetime.now()
 categories = {'museos': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv', 'salas_de_cine': "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv",
               'bibliotecas_populares': "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv"}
+# Tener en cuenta que las urls pueden cambiar en un futuro
 
 
 def getURL(url):
     return req.get(url)
 
 
-def saveFile(archive_name, url):
+def saveCSVFile(archive_name, url):
     with open(archive_name, 'w', encoding='utf-8') as file:
         writer = csv.writer(file)
-        for line in getURL(url).iter_lines():
+        for line in url.iter_lines():
             writer.writerow(line.decode('utf-8').split(','))
 
 
@@ -30,15 +31,14 @@ def createFolder(path):
 def setFoldersAndFiles(category):
     fullDate = x.strftime(category+'-%d-%m-%Y')
     monthName = x.strftime('%Y-%B')
+    archiveName = f'.\{category}\{monthName}\{fullDate}.csv'
     fullNameFile = f'.\{category}\{monthName}'
     createFolder(fullNameFile)
-    saveFile(fullDate, category)
+    for key in categories:
+        if (key == category):
+            valueCategory = categories[key]
+            saveCSVFile(archiveName, getURL(valueCategory))
 
 
-print(categories(0))
-
-# Tiene que decir “museos\2021-noviembre\museos-03-11-2021”
-
-# saveFile('museum.csv', museum)
-# saveFile('cinema.csv', cinema)
-# saveFile('popular_libraries.csv', popularLibraries)
+for key in categories:
+    setFoldersAndFiles(key)
