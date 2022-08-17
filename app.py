a82256f88e9186906d928093a9b1e822c12f7fa3
MorenaCaparros <morens217@gmail.com>
2022-08-17 11:58:07 -0300

    create dataframes from csv's

--- app.py
+++ app.py
@@ -6,17 +6,15 @@ import datetime
 import locale
 
 
+    
+    
+
 locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')  # cambia el idioma
 x = datetime.datetime.now()
 categories = {'museos': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv', 'salas_de_cine': "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv",
               'bibliotecas_populares': "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv"}
 
 # Tener en cuenta que las urls pueden cambiar en un futuro
-global df_library
-global df_museum
-global df_cinema
-
-
 def getURL(url):
     return req.get(url)
 
@@ -28,26 +26,39 @@ def saveCSVFile(archive_name, url):
             writer.writerow(line.decode('utf-8').split(','))
 
 
-def convertCSVToDataFrame(datafram, csv):
-    datafram = pd.read_csv(csv)
-
-
 def createFolder(path):
     os.makedirs(path, exist_ok=True)
 
 
 def setFoldersAndFiles(category):
+    global df_library
+    global df_museum
+    global df_cinema
+    
     fullDate = x.strftime(category+'-%d-%m-%Y')
     monthName = x.strftime('%Y-%B')
     archiveName = f'.\{category}\{monthName}\{fullDate}.csv'
     fullNameFile = f'.\{category}\{monthName}'
     createFolder(fullNameFile)
+    
     for key in categories:
         if (key == category):
             valueCategory = categories[key]
             saveCSVFile(archiveName, getURL(valueCategory))
 
-
+    if category == 'museos':
+        df_museum = convertCSVToDataFrame(archiveName)
+    elif category == 'salas_de_cine':
+        df_cinema = convertCSVToDataFrame(archiveName) 
+    elif category == 'bibliotecas_populares':
+        df_library = convertCSVToDataFrame(archiveName)
+   
+    
+def convertCSVToDataFrame(csv):
+    df = pd.read_csv(csv)
+    return df
+    
+    
 ###############
 
 # titulos que necesito:
@@ -66,7 +77,7 @@ def setFoldersAndFiles(category):
 #         header = next(csv_reader)
 #         print(header)
 
-
 for key in categories:
-
+    
     setFoldersAndFiles(key)
+    print (df_museum)
